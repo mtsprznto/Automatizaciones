715a3178e9df36022040f0c6f816a4a48e5631f0
Matias <lit.io30303@gmail.com>
2025-07-03 22:10:24 -0400

    update scraping

--- scraping_alltd/main.py
+++ scraping_alltd/main.py
@@ -30,66 +30,53 @@ async def main():
         FEATURED = "featured"
         TUTORIAL = "tutorial"
         
-        URL_SCRAPING = f'https://alltd.org/category/{FEATURED}/'
-        driver.get(URL_SCRAPING)
-        time.sleep(TIME_SLEEP)
-        
-
-        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        # time.sleep(TIME_SLEEP)
-        # new_height = driver.execute_script("return document.body.scrollHeight")
-        # time.sleep(TIME_SLEEP)
-
-        bloque_contenedor = driver.find_elements(By.XPATH, '//article[@class="cactus-post-item hentry"]')
-        time.sleep(TIME_SLEEP)
-
-
-        #-------------------------
-        # Procesamiento
-        #-------------------------
-        ITERACIONES = 90
-        articulos = []
-        for cont in range(0, ITERACIONES):
-            #print(bloque_contenedor[0].text)
-            primer_articulo = bloque_contenedor[cont]
-            enlace = primer_articulo.find_element(By.CSS_SELECTOR, "a").get_attribute('href')
-            #print(f"ENLACE: {enlace}")
-            titulo = primer_articulo.find_element(By.CSS_SELECTOR, "a").get_attribute('title')
-            #print(f"Titulo: {titulo}")
-            imagen = primer_articulo.find_element(By.CSS_SELECTOR, "img").get_attribute('src')
-            #print(f"URL de la imagen: {imagen}")
-            
+        POINT = [FEATURED, TUTORIAL]
+        for p in POINT:
+            print("-----------Inicio scraping-------------------")
+            print(f"Web: {p}")
+            URL_SCRAPING = f'https://alltd.org/category/{p}/'
+            print(f"Go to {URL_SCRAPING}")
+            driver.get(URL_SCRAPING)
             time.sleep(TIME_SLEEP)
-            
-            # Obtener el script que contiene el ID del video
-            script_content = primer_articulo.find_element(By.CSS_SELECTOR, "div.ct-icon-video script").get_attribute('textContent')
-            
-
-            #print(script_content)
-
-
-            respuesta = obtener_respuesta(script_content)
-            #print(respuesta)
-            articulo = {
-                "id": str(cont),  # Convert to string to match your example
-                "titulo": titulo,
-                "enlace_alltd": enlace,
-                "imagen": imagen,
-                "url_video": respuesta
-            }
-
-            articulos.append(articulo)
+            bloque_contenedor = driver.find_elements(By.XPATH, '//article[@class="cactus-post-item hentry"]')
             time.sleep(TIME_SLEEP)
-        
-        #print(articulos)
-        # Guardar en archivo
-        path_save = "./data_scraping/articulos.json"
-        with open(path_save, "w", encoding="utf-8") as archivo:
-            json.dump(articulos, archivo, indent=4, ensure_ascii=False)
-
-        print(f"Articulos guardados en {path_save}")
-
-
+            #-------------------------
+            # Procesamiento
+            #-------------------------
+            ITERACIONES = 90
+            articulos = []
+            
+            for cont in range(0, ITERACIONES):
+                primer_articulo = bloque_contenedor[cont]
+                enlace = primer_articulo.find_element(By.CSS_SELECTOR, "a").get_attribute('href')
+                titulo = primer_articulo.find_element(By.CSS_SELECTOR, "a").get_attribute('title')
+                autor = primer_articulo.find_element(By.CSS_SELECTOR,"span.fn").text
+                date = primer_articulo.find_element(By.CSS_SELECTOR, "time.entry-date.updated").text
+                imagen = primer_articulo.find_element(By.CSS_SELECTOR, "img").get_attribute('src')
+                time.sleep(TIME_SLEEP)
+                # Obtener el script que contiene el ID del video
+                script_content = primer_articulo.find_element(By.CSS_SELECTOR, "div.ct-icon-video script").get_attribute('textContent')
+                respuesta = obtener_respuesta(script_content)
+                articulo = {
+                    "id": str(cont),  # Convert to string to match your example
+                    "titulo": titulo,
+                    "autor": autor,
+                    "date":date,
+                    "enlace_alltd": enlace,
+                    "imagen": imagen,
+                    "url_video": respuesta
+                }
+                articulos.append(articulo)
+                time.sleep(TIME_SLEEP)
+                print(f"Bloque {cont} Completado")
+            # Guardar en archivo
+            path_save = f"./data_scraping/articulos_{p}.json"
+            with open(path_save, "w", encoding="utf-8") as archivo:
+                json.dump(articulos, archivo, indent=4, ensure_ascii=False)
+            print(f"Articulos guardados en {path_save}")
+            print(f"Terminando SCRAPING: {p}")
+            print("-"*80)
+            articulos = []
         driver.quit()
     except Exception as e:
         print(f"Error inesperado: {e}")
